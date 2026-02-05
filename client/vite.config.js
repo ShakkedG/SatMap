eaaccda67e86f507d0903233fe25e26b30f8be4b
ShakkedG <shagolan28@gmail.com>
2026-02-05 08:41:34 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  // חשוב: זה חייב להיות שם הריפו בדיוק (SatMap)
-  base: "/SatMap/",
   plugins: [vue()],
+  base: "/SatMap/",
 });
